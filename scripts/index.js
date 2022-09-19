b55426c3cef907259549e2380167def99067da5e
evgenia$ <den.gines007@gmail.com>
2022-09-19 17:32:45 +0300

    крайняя версия

---
+++ scripts/index.js
@@ -0,0 +1,79 @@
+const popup = document.querySelector('.popup');
+const openPopupButton = document.querySelector('.Kusto__edit-button');
+const closePopupButton = popup.querySelector('.popup__close-button');
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.Kusto__title');// Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.Kusto__text');// Воспользуйтесь инструментом .querySelector()
+let newTextName = document.querySelector('.popup__inpyt_type-name');
+let newTextJob = document.querySelector('.popup__inpyt_type-job');
+
+
+const togglePopup = () => {
+    popup.classList.toggle('popup_opened');
+}
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+    nameInput.textContent = newTextName.value;
+    jobInput.textContent = newTextJob.value;
+    togglePopup();
+}
+
+
+openPopupButton.addEventListener('click', () => {
+    togglePopup();
+    newTextName.value = nameInput.textContent;
+    newTextJob.value = jobInput.textContent;
+})
+
+closePopupButton.addEventListener('click', togglePopup);
+
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+// openPopupButton.addEventListener('click', () => {
+//     overlayEl.classList.add('overlay_opened');
+// })
+
+// closePopupButton.addEventListener('click', () => {
+//     overlayEl.classList.remove('overlay_opened');
+// })
+
+
+// document.addEventListener('click', (event) => {
+
+//     if (event.target.classList.contains('info-item__edit-button')) {
+//         toggleOverlay();
+//     }
+
+//     if (event.target.classList.contains('popup__close-button')) {
+//         toggleOverlay();
+//     }
+// });
+
+
+
+// const likes = document.querySelectorAll('.card__like');
+
+// const toggleClass = (e) => {
+//     e.classList.toggle('card__like_active');
+// }
+
+// likes.addEventListener('click', (e) => {
+// debugger;    toggleClass(e.currentTarget);
+// })
