5e30c8bd6b0622d2c7fab20eb8941c17e322b9fc
den007 <den007@gmail.com>
2023-04-10 16:05:18 +0300

    цууу

--- scripts/index.js
+++ scripts/index.js
@@ -144,3 +144,10 @@ initialCards.forEach((item) => {
 
 export { openPopup };
 console.log('Hello, World!')
+
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
